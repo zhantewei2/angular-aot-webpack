11f5683c0a3e569e95c53818ed13e7e40860c7cd
zhantewei <zhantewei@gmail.com>
2017-02-06 22:42:20 +0800

    Update app.component.ts

--- src/app.component.ts
+++ src/app.component.ts
@@ -4,9 +4,10 @@ import {Component} from '@angular/core';
 	selector:'my-app',
 	template:`
 		<h1>angular-aot-webpack</h1>
-
-		<div class='btn btn-default'><a routerLink='/1'>load</a></div>
-		<div class='btn btn-default'><a routerLink='/2'>Lazy load</a></div>
+		<div class='btn-group'>
+			<a class='btn btn-outline-secondary' routerLink='1' routerLinkActive='active'>load</a>
+			<a class='btn btn-outline-secondary' routerLink='2' routerLinkActive='active'>Lazy load</a>
+		</div>
 		<router-outlet> </router-outlet>
 	`
 
