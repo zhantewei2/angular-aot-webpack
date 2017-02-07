c71a7f7826f8e4fadb2d41f93988219294cf5dbd
zhantewei <zhantewei@gmail.com>
2017-02-07 13:50:52 +0800

    Add files via upload

--- src/app.component.ts
+++ src/app.component.ts
@@ -3,12 +3,27 @@ import {Component} from '@angular/core';
 @Component({
 	selector:'my-app',
 	template:`
-		<h1>angular-aot-webpack</h1>
-		<div class='btn-group'>
-			<a class='btn btn-outline-secondary' routerLink='1' routerLinkActive='active'>load</a>
-			<a class='btn btn-outline-secondary' routerLink='2' routerLinkActive='active'>Lazy load</a>
+		<nav class='navbar bg-inverse' style='color:white'>
+			<i>angular-aot-webpack</i>
+			<i class='align-self-end'>---by zhantewei</i>
+		</nav>
+		<br>
+		<div class='card '>
+			<div class='card-header'>
+				<div class='nav nav-tabs card-header-tabs'>
+					<a class='nav-item nav-link' routerLink='1' routerLinkActive='active'>load</a>
+					<a class='nav-item nav-link' routerLink='2' routerLinkActive='active'>Lazy load</a>
+				</div>
+			</div>
+			<div class='card-block'>
+				<div class='card-text'>
+				<router-outlet> </router-outlet>
+				</div>
+			</div>
+			<div class='card-footer Text-right'>
+				<span>Router Card</span>
+			</div>
 		</div>
-		<router-outlet> </router-outlet>
 	`
 
 })
