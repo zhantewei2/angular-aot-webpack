8f55fdd5da7baf9bbe6c4167e4641166ded03501
zhantewei <zhantewei@gmail.com>
2017-02-07 12:52:06 +0800

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
-import {CommonModule} from '@angular/common';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './router.module';
 import {Child2Component} from './child2/child2.component';
